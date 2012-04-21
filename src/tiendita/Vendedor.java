7e9e4a8e81f1a2ab3bce56dccbdb714148d9571d
ubuntu <ubuntu@ubuntu>
2012-04-20 19:22:37 -0500

    El vendedor ahora puede realizar una venta con el metodo "realizarVenta". El cual practicamente recibe la jTable ya que esta esta computesa por Objetos Vector.

--- src/tiendita/Vendedor.java
+++ src/tiendita/Vendedor.java
@@ -5,6 +5,9 @@
 package tiendita;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Vector;
 
 /**
  *
@@ -18,5 +21,38 @@ public class Vendedor {
     
     public Vendedor(String nombre){
         this.nombre = nombre;
+    }
+    
+    public void realizaVenta(Vector listaDeProductos, Connection conn)
+    throws TienditaException{
+        //Buscar una mejor estructura de datos para vector.
+        if (conn == null) 
+            throw new TienditaException("Error. No hay conexion con la base de datos.");
+        
+        String update = "update Inventario set existencias = ? where upc = ?";
+        Vector v;
+        String upc;
+        int cantidad;
+        try{
+            PreparedStatement ps = 
+                    (PreparedStatement) conn.prepareStatement(update);
+            
+            for (int i = 0; i< listaDeProductos.size(); i++){
+                v = (Vector) listaDeProductos.get(i);
+                upc = (String) v.get(0);
+                cantidad =  (Integer) v.get(2);
+                
+                ps.setInt(1,cantidad);
+                ps.setString(2,upc);
+                
+                ps.addBatch();
+            }
+            
+            ps.executeBatch();
+            
+            
+        }catch(SQLException e){
+            throw new TienditaException("Error. " + e.getMessage());
+        }
     }
 }
